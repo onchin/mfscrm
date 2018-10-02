23e6c48a1e96f252323848cb7bd62b14ca5de561
onchin <ochin@unomaha.edu>
2018-10-02 12:28:53 -0500

    Initial commit

---
+++ crm/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import *
+from .forms import *
+
+
+def customer_list(request):
+    customer = Customer.objects.filter(created_date__lte=timezone.now())
+    return render(request, 'crm/customer_list.html', {'customers': customer})
+
