984d2dd1258fe0270effa91d657ec65fd4d7ed66
onchin <ochin@unomaha.edu>
2018-10-05 02:37:23 -0500

    App completed - Initial deploy to heroku

--- crm/views.py
+++ crm/views.py
@@ -5,6 +5,7 @@ from .models import *
 from .forms import *
 from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
+from django.db.models import Sum
 
 
 now = timezone.now()
@@ -145,3 +146,18 @@ def product_delete(request, pk):
     product = get_object_or_404(Product, pk=pk)
     product.delete()
     return redirect('crm:product_list')
+
+
+@login_required
+def summary(request, pk):
+    customer = get_object_or_404(Customer, pk=pk)
+    customers = Customer.objects.filter(created_date__lte=timezone.now())
+    services = Service.objects.filter(cust_name=pk)
+    products = Product.objects.filter(cust_name=pk)
+    sum_service_charge = Service.objects.filter(cust_name=pk).aggregate(Sum('service_charge'))
+    sum_product_charge = Product.objects.filter(cust_name=pk).aggregate(Sum('charge'))
+    return render(request, 'crm/summary.html', {'customers': customers,
+                                                'products': products,
+                                                'services': services,
+                                                'sum_service_charge': sum_service_charge,
+                                                'sum_product_charge': sum_product_charge, })
